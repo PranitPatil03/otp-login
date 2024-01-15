6d08aa48a9564990e50be603010e2b8de7575e5d
Pranit <patilpranit3112@gmail.com>
2024-01-15 20:24:11 +0530

    otp UI Completed

--- src/components/otp-input.jsx
+++ src/components/otp-input.jsx
@@ -1,36 +1,46 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useRef, useState } from "react";
 
-const OTPInput = ({ length, onOtpSubmit = () => {} }) => {
-  const [otp, setOtp] = useState(new Array(length).fill(" "));
-
+const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
+  const [otp, setOtp] = useState(new Array(length).fill(""));
   const inputRefs = useRef([]);
 
   const handleChange = (index, e) => {
     const value = e.target.value;
-
-    if (isNaN(value)) {
-      return;
-    }
+    if (isNaN(value)) return;
 
     const newOtp = [...otp];
-
     newOtp[index] = value.substring(value.length - 1);
-
     setOtp(newOtp);
 
-    const combineOtp = newOtp.join("");
-
-    if (combineOtp.length === length) onOtpSubmit(combineOtp);
+    const combinedOtp = newOtp.join("");
+    if (combinedOtp.length === length) onOtpSubmit(combinedOtp);
 
     if (value && index < length - 1 && inputRefs.current[index + 1]) {
       inputRefs.current[index + 1].focus();
+
+      inputRefs.current[otp.indexOf("")].focus();
     }
   };
 
-  const handleClick = () => {};
+  const handleClick = (index) => {
+    inputRefs.current[index].setSelectionRange(1, 1);
 
-  const handleKeyDown = () => {};
+    if (index > 0 && !otp[index - 1]) {
+      inputRefs.current[otp.indexOf("")].focus();
+    }
+  };
+
+  const handleKeyDown = (index, e) => {
+    if (
+      e.key === "Backspace" &&
+      !otp[index] &&
+      index > 0 &&
+      inputRefs.current[index - 1]
+    ) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
 
   useEffect(() => {
     if (inputRefs.current[0]) {
@@ -40,13 +50,13 @@ const OTPInput = ({ length, onOtpSubmit = () => {} }) => {
 
   return (
     <div>
-      {otp.map((otp, index) => {
+      {otp.map((value, index) => {
         return (
           <input
             key={index}
             type="text"
             ref={(input) => (inputRefs.current[index] = input)}
-            value={otp}
+            value={value}
             onChange={(e) => handleChange(index, e)}
             onClick={() => handleClick(index)}
             onKeyDown={(e) => handleKeyDown(index, e)}
@@ -58,4 +68,4 @@ const OTPInput = ({ length, onOtpSubmit = () => {} }) => {
   );
 };
 
-export default OTPInput;
+export default OtpInput;
